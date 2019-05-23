cc7534d4e0cf19e2056af65181672f9fa28bb3a0
tourman <8002615@gmail.com>
2019-05-23 17:57:39 +0300

    reducer tests: SEARCH_RESPONSE

--- src/Search/reducer.js
+++ src/Search/reducer.js
@@ -1,6 +1,7 @@
 import produce from 'immer';
 
 const SEARCH_REQUEST = 'SEARCH_REQUEST';
+const SEARCH_RESPONSE = 'SEARCH_RESPONSE';
 
 const initialState = {
   busy: false,
@@ -16,6 +17,28 @@ const initialState = {
   },
 };
 
+function splitName(name, request) {
+  const typeMap = [
+    'normal',
+    'selected',
+    'normal',
+  ];
+  const re = new RegExp(`^(.*)(${request})(.*)$`);
+  const match = name.match(re);
+  if (!match) {
+    throw new Error(`No matches for name (${name}) and request (${request})`);
+  }
+  const tokens = [].slice.call(match, 1);
+  const split = tokens
+    .map((text, index) => ({
+      type: typeMap[index],
+      text,
+    }))
+    .filter(item => !!item.text)
+  ;
+  return split;
+}
+
 export default function search(prevState = initialState, action) {
   const state = produce(prevState, draft => {
     switch(action.type) {
@@ -23,6 +46,15 @@ export default function search(prevState = initialState, action) {
         draft.busy = true;
         draft.reset.available = true;
         draft.input.string = action.payload.request;
+        break;
+      }
+      case SEARCH_RESPONSE: {
+        draft.busy = false;
+        draft.list.available = true;
+        draft.list.items = action.payload.response.results.map(({ name }) => ({
+          name,
+          split: splitName(name, draft.input.string),
+        }));
         break;
       }
       default: {
--- src/Search/reducer.test.js
+++ src/Search/reducer.test.js
@@ -15,18 +15,18 @@ const initialState = {
   },
 };
 
-const cloneAndMerge = merge.bind(null, {}, initialState);
+const cloneAndMerge = (...extensions) => merge({}, initialState, ...extensions);
 
 describe('search reducer', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
   });
 
-  it('should handle search string', () => {
+  it('should handle request by search string', () => {
     expect(reducer(initialState, {
       type: 'SEARCH_REQUEST',
       payload: {
-        request: 'aboo',
+        request: 'oo',
       },
     })).toEqual(cloneAndMerge({
       busy: true,
@@ -34,8 +34,177 @@ describe('search reducer', () => {
         available: true,
       },
       input: {
-        string: 'aboo',
+        string: 'oo',
       },
     }));
+  });
+
+  it('should handle search response', () => {
+    // Arrange
+    const state = cloneAndMerge({
+      busy: true,
+      reset: {
+        available: true,
+      },
+      input: {
+        string: 'oo',
+      },
+    });
+    const payload = {
+      type: 'SEARCH_RESPONSE',
+      payload: {
+        response: {
+          count: 3,
+          next: null,
+          previous: null,
+          results: [
+            {
+              name: 'Naboo',
+              rotation_period: '26',
+              orbital_period: '312',
+              diameter: '12120',
+              climate: 'temperate',
+              gravity: '1 standard',
+              terrain: 'grassy hills, swamps, forests, mountains',
+              surface_water: '12',
+              population: '4500000000',
+              residents: [
+                'https://swapi.co/api/people/3/',
+                'https://swapi.co/api/people/21/',
+                'https://swapi.co/api/people/36/',
+                'https://swapi.co/api/people/37/',
+                'https://swapi.co/api/people/38/',
+                'https://swapi.co/api/people/39/',
+                'https://swapi.co/api/people/42/',
+                'https://swapi.co/api/people/60/',
+                'https://swapi.co/api/people/61/',
+                'https://swapi.co/api/people/66/',
+                'https://swapi.co/api/people/35/'
+              ],
+              films: [
+                'https://swapi.co/api/films/5/',
+                'https://swapi.co/api/films/4/',
+                'https://swapi.co/api/films/6/',
+                'https://swapi.co/api/films/3/'
+              ],
+              created: '2014-12-10T11:52:31.066000Z',
+              edited: '2014-12-20T20:58:18.430000Z',
+              url: 'https://swapi.co/api/planets/8/'
+            },
+            {
+              name: 'Dantoo/^$ine',
+              rotation_period: '25',
+              orbital_period: '378',
+              diameter: '9830',
+              climate: 'temperate',
+              gravity: '1 standard',
+              terrain: 'oceans, savannas, mountains, grasslands',
+              surface_water: 'unknown',
+              population: '1000',
+              residents: [],
+              films: [],
+              created: '2014-12-10T17:23:29.896000Z',
+              edited: '2014-12-20T20:58:18.461000Z',
+              url: 'https://swapi.co/api/planets/25/'
+            },
+            {
+              name: 'Tatooine',
+              rotation_period: '23',
+              orbital_period: '304',
+              diameter: '10465',
+              climate: 'arid',
+              gravity: '1 standard',
+              terrain: 'desert',
+              surface_water: '1',
+              population: '200000',
+              residents: [
+                'https://swapi.co/api/people/1/',
+                'https://swapi.co/api/people/2/',
+                'https://swapi.co/api/people/4/',
+                'https://swapi.co/api/people/6/',
+                'https://swapi.co/api/people/7/',
+                'https://swapi.co/api/people/8/',
+                'https://swapi.co/api/people/9/',
+                'https://swapi.co/api/people/11/',
+                'https://swapi.co/api/people/43/',
+                'https://swapi.co/api/people/62/'
+              ],
+              films: [
+                'https://swapi.co/api/films/5/',
+                'https://swapi.co/api/films/4/',
+                'https://swapi.co/api/films/6/',
+                'https://swapi.co/api/films/3/',
+                'https://swapi.co/api/films/1/'
+              ],
+              created: '2014-12-09T13:50:49.641000Z',
+              edited: '2014-12-21T20:48:04.175778Z',
+              url: 'https://swapi.co/api/planets/1/'
+            }
+          ]
+        },
+      },
+    };
+    const expected = cloneAndMerge(state, {
+      busy: false,
+      reset: {
+        available: true,
+      },
+      list: {
+        available: true,
+        items: [
+          {
+            name: 'Naboo',
+            split: [
+              {
+                type: 'normal',
+                text: 'Nab',
+              },
+              {
+                type: 'selected',
+                text: 'oo',
+              },
+            ],
+          },
+          {
+            name: 'Dantoo/^$ine',
+            split: [
+              {
+                type: 'normal',
+                text: 'Dant',
+              },
+              {
+                type: 'selected',
+                text: 'oo',
+              },
+              {
+                type: 'normal',
+                text: '/^$ine',
+              },
+            ],
+          },
+          {
+            name: 'Tatooine',
+            split: [
+              {
+                type: 'normal',
+                text: 'Tat',
+              },
+              {
+                type: 'selected',
+                text: 'oo',
+              },
+              {
+                type: 'normal',
+                text: 'ine',
+              },
+            ],
+          }
+        ],
+      },
+    });
+    // Act
+    const result = reducer(state, payload);
+    // Assert
+    expect(result).toEqual(expected);
   });
 });
