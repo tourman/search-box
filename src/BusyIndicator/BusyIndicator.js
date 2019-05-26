5e9f89f26d26f564002360e22acec9ed71a9c983
tourman <8002615@gmail.com>
2019-05-26 22:07:00 +0300

    material-ui: busy icon

--- src/BusyIndicator/BusyIndicator.js
+++ src/BusyIndicator/BusyIndicator.js
@@ -1,19 +1,27 @@
 import React from 'react';
 
-import SyncIcon from '@material-ui/icons/Sync';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 
 const busyMap = new Map()
-  .set(true,  SyncIcon)
-  .set(false, SearchIcon)
+  .set(true,  {
+    Icon: CircularProgress,
+    iconProps: {
+      size: 24,
+    },
+  })
+  .set(false, {
+    Icon: SearchIcon,
+    iconProps: {},
+  })
 ;
 
 export default function BusyIndicator(props) {
-  const Icon = busyMap.get(props.busy);
+  const { Icon, iconProps } = busyMap.get(props.busy);
   return (
     <IconButton>
-      <Icon />
+      <Icon {...iconProps} />
     </IconButton>
   );
 };
